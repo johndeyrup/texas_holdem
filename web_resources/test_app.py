190519ab7189dc5870c92e0df584b52b4af710da
johndeyrup <john.deyrup@gmail.com>
2015-05-11 00:03:57 +0900

    web resources
    still figuring out how to use flask

--- web_resources/test_app.py
+++ web_resources/test_app.py
@@ -3,18 +3,16 @@ Created on May 10, 2015
 
 @author: arilab
 '''
-import os
 from flask import Flask, render_template
 app = Flask(__name__)
-print(os.listdir(app.template_folder))
 
 @app.route('/')
-def index():
-    return 'Index Page TEst'
-
-@app.route('/game_page')
-def game_page():
+def Login():
     return render_template('index.html')
+
+@app.route('/game')
+def Game():
+    return render_template('game.html')
 
 if __name__ == '__main__':
     app.run(debug=True)

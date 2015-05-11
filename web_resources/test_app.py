3142939071e4a3436d8d3fec12d4b6afd344dfe6
John <john.deyrup@gmail.com>
2015-05-11 23:16:11 +0900

    web resources
    trying to figure out html and javascript

--- web_resources/test_app.py
+++ web_resources/test_app.py
@@ -3,15 +3,34 @@ Created on May 10, 2015
 
 @author: arilab
 '''
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
+#Dictonary of users and passworkds
+valid_pass = {'bob':'smith', 'sarah':'micheals'}
+
+#Default login page see index.html, this should be changed to login.html once homepage is created
 @app.route('/')
 def Login():
     return render_template('index.html')
 
+#Do something with the results from the form, the variables see index.html user and pass, these get
+#stored in user and password. In the index.html login causes this method to be run
+@app.route('/', methods=['POST'])
+def check_password():
+    user = request.form['user']
+    password = request.form['pass']
+    try:
+        if valid_pass[user] == password:
+            return redirect(url_for('game'))
+        else:
+            return 'Invalid username or password'
+    except:
+        return 'Invalid username or password'
+
+#Renders gamescreen
 @app.route('/game')
-def Game():
+def game():
     return render_template('game.html')
 
 if __name__ == '__main__':
